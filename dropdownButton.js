0dca7b52514de7f454476dfa48262af39cf8fbe5
CocytusDEDI <84678844+CocytusDEDI@users.noreply.github.com>
2023-11-09 22:14:18 +0000

    Update dropdownButton.js

--- dropdownButton.js
+++ dropdownButton.js
@@ -1,12 +1,18 @@
-document.getElementById("dropdownButton").addEventListener("click", dropdown);
+const resources = document.querySelectorAll('.dropdownResource');
 
-function dropdown() {
-    if (document.getElementById("dropdownTest").outerHTML == "<div id=\"dropdownTest\"></div>") {
-        document.getElementById("dropdownTest").outerHTML = "<div class=\"dropdown\" id=\"dropdownTest\"><span>This is a test<br><br>This is a another test</span></div>"
-        document.getElementById("dropdownSymbol").innerHTML = "<path d=\"m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z\" fill=\"#ddd\"></path>"
+resources.forEach((resource, index) => {
+  resource.addEventListener('click', function() {
+    dropdown(`dropdown${index + 1}`, `dropdownSymbol${index + 1}`);
+  });
+});
+
+function dropdown(dropdownID, dropdownSymbolID) {
+    if (document.getElementById(dropdownID).outerHTML == "<div id=\"" + dropdownID + "\"></div>") {
+        document.getElementById(dropdownID).outerHTML = "<div class=\"dropdown\" id=\"" + dropdownID + "\"><span>This is a test<br><br>This is a another test</span></div>"
+        document.getElementById(dropdownSymbolID).innerHTML = "<path d=\"m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z\" fill=\"#ddd\"></path>"
     }
     else {
-        document.getElementById("dropdownTest").outerHTML = "<div id=\"dropdownTest\"></div>"
-        document.getElementById("dropdownSymbol").innerHTML = "<path d=\"M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z\" fill=\"#ddd\"></path>"
+        document.getElementById(dropdownID).outerHTML = "<div id=\"" + dropdownID + "\"></div>"
+        document.getElementById(dropdownSymbolID).innerHTML = "<path d=\"M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z\" fill=\"#ddd\"></path>"
     }
 }
